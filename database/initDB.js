cd32f638b1a2b759f485ff32fbab733bfdcc3d8c
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-09 19:45:05 +0200

    init db

--- database/initDB.js
+++ database/initDB.js
@@ -7,13 +7,34 @@ const initDB = async () => {
         const pool = getPool();
         const comp = await pool.query(`
             CREATE TABLE IF NOT EXISTS users(
-                id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                id VARCHAR(36) NOT NULL,
                 username VARCHAR(50),
                 password VARCHAR(50),
                 email VARCHAR(50),
                 biography VARCHAR(255),
                 photo VARCHAR(255),
-                creation_date DATE
+                creation_date DATE,
+                PRIMARY KEY (id)
+            )
+        `)
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS folders(
+                id VARCHAR(36) NOT NULL,
+                iduser VARCHAR(36) NOT NULL,
+                name VARCHAR(50),
+                location VARCHAR(50),
+                PRIMARY KEY (id),
+                FOREIGN KEY (iduser) REFERENCES users (id)
+            )
+        `)
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS files(
+                id VARCHAR(36) NOT NULL,
+                idfolder VARCHAR(36) NOT NULL,
+                name VARCHAR(50),
+                creation_date DATE,
+                PRIMARY KEY (id),
+                FOREIGN KEY (idfolder) REFERENCES folders (id)
             )
         `)
         if (!comp[0].warningStatus) {
