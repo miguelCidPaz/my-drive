38c012bc79070f4b95a18db8fffd199f123f5799
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-09 20:00:57 +0200

    impl jwt

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userExtractor = require('../middlewares/userExtractor')
 
 router.post('/register', require('../controllers/user/register'));
-router.post('/login', userExtractor, require('../controllers/user/login'));
+router.post('/login', require('../controllers/user/login'));
 router.get('/reconnect', userExtractor, require('../controllers/user/reconnect'));
 router.post('/modify', userExtractor, require('../controllers/user/modify'));
 
