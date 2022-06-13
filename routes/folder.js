9c361912c0eebe53ef595181f236af646c67b49e
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-13 04:02:50 +0200

    securization and README

--- routes/folder.js
+++ routes/folder.js
@@ -3,6 +3,7 @@ const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
 router.post('/create-folder', upload.single('file'), require('../controllers/folders/createFolder'))
-router.post('/rename', require('../controllers/folders/renameFolder'))
+router.post('/rename', require('../controllers/folders/renameFolder'));
+router.post('/delete-folder', require('../controllers/folders/deleteFolder'));
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const userExtractor = require('../middlewares/userExtractor')
 
 router.use('/usr', require('./user'));
-router.use('/folder', require('./folder'));
-router.use('/files', require('./files'));
+router.use('/folder', userExtractor, require('./folder'));
+router.use('/files', userExtractor, require('./files'));
 
 module.exports = router
