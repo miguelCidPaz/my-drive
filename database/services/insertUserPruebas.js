b9906cb95cc4163b78e1615f5a486e1fd9be02ea
Miguel Cid <miguel.cid.paz@gmail.com>
2022-07-13 23:01:00 +0200

    md5 in populateDB

--- database/services/insertUserPruebas.js
+++ database/services/insertUserPruebas.js
@@ -1,12 +1,14 @@
 const { v4 } = require('uuid')
 const insertUser = require('../../repositories/users/insertUser')
+const md5 = require('md5');
 
 const insertUserPruebas = async () => {
+    const psw = md5(1234)
     try {
         const newUser = {
             id: v4(),
             username: 'miguel',
-            password: '1234',
+            password: psw,
             email: 'miguel.cid.paz@gmail.com',
             biography: 'soy el user de pruebas',
             user_active: 1,
